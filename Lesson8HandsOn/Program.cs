fa93f7914d53ee829a00eb60eb26390e842c2926
unknown <Omer>
2017-06-05 14:08:01 -0700

    Lesson 8 Start

---
+++ Lesson8HandsOn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson8HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
